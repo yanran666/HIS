242eea3287e445e6b116e594acdb74304ed70bc0
sonicyouth7 <1625159433@qq.com>
2024-07-08 10:54:12 +0800

    logo

---
+++ YGT/web/RuoYi-Vue3-master/src/views/appointment/appointment/index.vue
@@ -0,0 +1,491 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-form
+        ref="formRef"
+        :model="formData"
+        :rules="rules"
+        size="small"
+        label-width="78px"
+        label-position="left"
+      >
+        <el-col :span="24">
+          <el-row gutter="15">
+            <el-col :span="8">
+              <el-form-item label="病历号" prop="field104">
+                <el-input
+                  v-model="formData.field104"
+                  type="text"
+                  placeholder="请输入病历号"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="姓名" prop="field103">
+                <el-input
+                  v-model="formData.field103"
+                  type="text"
+                  placeholder="请输入姓名"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="性别" prop="field102">
+                <el-select
+                  v-model="formData.field102"
+                  placeholder="请输入性别"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field102Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="24">
+          <el-row gutter="15">
+            <el-col :span="8">
+              <el-form-item label="年龄" prop="field106">
+                <el-input
+                  v-model="formData.field106"
+                  type="text"
+                  placeholder="请输入年龄"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="出生日期" prop="field107">
+                <el-date-picker
+                  type="text"
+                  v-model="formData.field107"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  :style="{ width: '100%' }"
+                  placeholder="请输入出生日期"
+                  clearable
+                ></el-date-picker>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="身份证号" prop="field108">
+                <el-input
+                  v-model="formData.field108"
+                  type="text"
+                  placeholder="请输入身份证号"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="23">
+          <el-row gutter="15">
+            <el-col :span="24">
+              <el-form-item label="家庭住址" prop="field110">
+                <el-input
+                  v-model="formData.field110"
+                  type="text"
+                  placeholder="请输入家庭住址"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="23">
+          <el-row gutter="15">
+            <el-col :span="8">
+              <el-form-item label="结算类别" prop="field113">
+                <el-select
+                  v-model="formData.field113"
+                  placeholder="请选择结算类别"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field113Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="看诊日期" prop="field115">
+                <el-date-picker
+                  v-model="formData.field115"
+                  format="YYYY-MM-DD"
+                  value-format="YYYY-MM-DD"
+                  :style="{ width: '100%' }"
+                  placeholder="请选择看诊日期"
+                  clearable
+                  :disabled="true"
+                ></el-date-picker>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="午别" prop="field114">
+                <el-select
+                  v-model="formData.field114"
+                  placeholder="请选择午别"
+                  :disabled="true"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field114Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="23">
+          <el-row gutter="15">
+            <el-col :span="8">
+              <el-form-item label="挂号科室" prop="field120">
+                <el-select
+                  v-model="formData.field120"
+                  placeholder="请选择挂号科室"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field120Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="号别" prop="field136">
+                <el-select
+                  v-model="formData.field136"
+                  placeholder="请输入号别"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field136Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="看诊医生" prop="field122">
+                <el-select
+                  v-model="formData.field122"
+                  placeholder="请选择看诊医生"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field122Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="23">
+          <el-row gutter="15">
+            <el-col :span="8">
+              <el-form-item label="初始号额" prop="field124">
+                <el-input
+                  v-model="formData.field124"
+                  type="text"
+                  placeholder="请选择初始号额"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="已用号额" prop="field138">
+                <el-input
+                  v-model="formData.field138"
+                  type="text"
+                  placeholder="请输入已用号额"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="病历本" prop="field126" required>
+                <el-switch v-model="formData.field126"></el-switch>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="23">
+          <el-row gutter="15">
+            <el-col :span="8">
+              <el-form-item label="应收金额" prop="field128">
+                <el-input
+                  v-model="formData.field128"
+                  type="text"
+                  placeholder="请选择应收金额"
+                  clearable
+                  :style="{ width: '100%' }"
+                ></el-input>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="收费方式" prop="field130">
+                <el-select
+                  v-model="formData.field130"
+                  placeholder="请选择收费方式"
+                  clearable
+                  :style="{ width: '100%' }"
+                >
+                  <el-option
+                    v-for="(item, index) in field130Options"
+                    :key="index"
+                    :label="item.label"
+                    :value="item.value"
+                    :disabled="item.disabled"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="24">
+          <el-form-item>
+            <el-button type="primary" @click="submitForm">提交</el-button>
+            <el-button @click="resetForm">重置</el-button>
+          </el-form-item>
+        </el-col>
+      </el-form>
+    </el-row>
+  </div>
+</template>
+<script setup>
+import { ElMessage } from "element-plus";
+const formRef = ref();
+const data = reactive({
+  formData: {
+    field104: undefined,
+    field103: undefined,
+    field102: 2,
+    field106: undefined,
+    field107: null,
+    field108: undefined,
+    field110: undefined,
+    field113: undefined,
+    field115: "2024-07-20",
+    field114: 1,
+    field120: undefined,
+    field136: 1,
+    field122: undefined,
+    field124: 1,
+    field138: 0,
+    field126: true,
+    field128: 50,
+    field130: 1,
+  },
+  rules: {
+    field104: [
+      {
+        required: true,
+        message: "请输入病历号",
+        trigger: "blur",
+      },
+    ],
+    field103: [
+      {
+        required: true,
+        message: "请输入姓名",
+        trigger: "blur",
+      },
+    ],
+    field102: [
+      {
+        required: true,
+        message: "请输入性别",
+        trigger: "change",
+      },
+    ],
+    field106: [],
+    field107: [],
+    field108: [
+      {
+        required: true,
+        message: "请输入身份证号",
+        trigger: "blur",
+      },
+    ],
+    field110: [],
+    field113: [],
+    field115: [
+      {
+        required: true,
+        message: "请选择看诊日期",
+        trigger: "change",
+      },
+    ],
+    field114: [],
+    field120: [
+      {
+        required: true,
+        message: "请选择挂号科室",
+        trigger: "change",
+      },
+    ],
+    field136: [],
+    field122: [],
+    field124: [
+      {
+        required: true,
+        message: "请选择初始号额",
+        trigger: "blur",
+      },
+    ],
+    field138: [
+      {
+        required: true,
+        message: "请输入已用号额",
+        trigger: "blur",
+      },
+    ],
+    field128: [
+      {
+        required: true,
+        message: "请选择应收金额",
+        trigger: "blur",
+      },
+    ],
+    field130: [
+      {
+        required: true,
+        message: "请选择收费方式",
+        trigger: "change",
+      },
+    ],
+  },
+});
+const { formData, rules } = toRefs(data);
+const field102Options = ref([
+  {
+    label: "男",
+    value: 1,
+  },
+  {
+    label: "女",
+    value: 2,
+  },
+]);
+const field113Options = ref([
+  {
+    label: "自费",
+    value: 1,
+  },
+  {
+    label: "市医保",
+    value: 2,
+  },
+]);
+const field114Options = ref([
+  {
+    label: "上午",
+    value: 1,
+  },
+  {
+    label: "下午",
+    value: 2,
+  },
+]);
+const field120Options = ref([
+  {
+    label: "选项一",
+    value: 1,
+  },
+  {
+    label: "选项二",
+    value: 2,
+  },
+]);
+const field136Options = ref([
+  {
+    label: "普通号",
+    value: 1,
+  },
+  {
+    label: "专家号",
+    value: 2,
+  },
+]);
+const field122Options = ref([
+  {
+    label: "上午",
+    value: 1,
+  },
+  {
+    label: "下午",
+    value: 2,
+  },
+]);
+const field130Options = ref([
+  {
+    label: "医保卡",
+    value: 1,
+  },
+  {
+    label: "现金",
+    value: 2,
+  },
+]);
+/**
+ * @name: 表单提交
+ * @description: 表单提交方法
+ * @return {*}
+ */
+function submitForm() {
+  formRef.value.validate((valid) => {
+    if (!valid) return;
+    // TODO 提交表单
+  });
+}
+/**
+ * @name: 表单重置
+ * @description: 表单重置方法
+ * @return {*}
+ */
+function resetForm() {
+  formRef.value.resetFields();
+}
+</script>
+<style></style>
--- YGT/web/RuoYi-Vue3-master/src/views/index.vue
+++ YGT/web/RuoYi-Vue3-master/src/views/index.vue
@@ -41,7 +41,7 @@
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
         <h2>若依后台管理框架</h2>
         <p>
-          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了若依管理系统，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。
+          一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了医管通，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。
         </p>
         <p>
           <b>当前版本:</b> <span>v{{ version }}</span>
@@ -118,11 +118,18 @@
               >
             </p>
             <p>
-              <i class="el-icon-user-solid"></i> QQ群：<s> 满937441 </s> <s> 满887144332 </s>
-              <s> 满180251782 </s> <s> 满104180207 </s> <s> 满186866453 </s> <s> 满201396349 </s>
-              <s> 满101456076 </s> <s> 满101539465 </s> <s> 满264312783 </s> <s> 满167385320 </s> 
-              <s> 满104748341 </s> <s> 满160110482 </s> <s> 满170801498 </s> <s> 满108482800 </s> 
-              <s> 满101046199 </s> <s> 满136919097 </s> <s> 满143961921 </s> <s> 满174951577 </s> <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=Lj0nHRAPD5CZv1jTOciuxXVloBJLS2Lp&authKey=Q2RxC6%2Ffxney9yOGBY0sDJxFhX9b7o1FRY1bsESmkbcZ4PFt6Vx92FpVo9O1u9p4&noverify=0&group_code=161281055" target="_blank">161281055</a>
+              <i class="el-icon-user-solid"></i> QQ群：<s> 满937441 </s>
+              <s> 满887144332 </s> <s> 满180251782 </s> <s> 满104180207 </s>
+              <s> 满186866453 </s> <s> 满201396349 </s> <s> 满101456076 </s>
+              <s> 满101539465 </s> <s> 满264312783 </s> <s> 满167385320 </s>
+              <s> 满104748341 </s> <s> 满160110482 </s> <s> 满170801498 </s>
+              <s> 满108482800 </s> <s> 满101046199 </s> <s> 满136919097 </s>
+              <s> 满143961921 </s> <s> 满174951577 </s>
+              <a
+                href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=Lj0nHRAPD5CZv1jTOciuxXVloBJLS2Lp&authKey=Q2RxC6%2Ffxney9yOGBY0sDJxFhX9b7o1FRY1bsESmkbcZ4PFt6Vx92FpVo9O1u9p4&noverify=0&group_code=161281055"
+                target="_blank"
+                >161281055</a
+              >
             </p>
             <p>
               <i class="el-icon-chat-dot-round"></i> 微信：<a
@@ -263,7 +270,9 @@
                 <li>修复table中更多按钮切换主题色未生效修复问题</li>
                 <li>修复某些特性的环境生成代码变乱码TXT文件问题</li>
                 <li>修复代码生成图片/文件/单选时选择必填无法校验问题</li>
-                <li>修复某些特性的情况用户编辑对话框中角色和部门无法修改问题</li>
+                <li>
+                  修复某些特性的情况用户编辑对话框中角色和部门无法修改问题
+                </li>
                 <li>其他细节优化</li>
               </ol>
             </el-collapse-item>
@@ -324,7 +333,9 @@
                 <li>新增获取不带后缀文件名称方法</li>
                 <li>新增获取配置文件中的属性值方法</li>
                 <li>新增内容编码/解码方便插件集成使用</li>
-                <li>字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）</li>
+                <li>
+                  字典类型必须以字母开头，且只能为（小写字母，数字，下滑线）
+                </li>
                 <li>优化设置分页参数默认值</li>
                 <li>优化对空字符串参数处理的过滤</li>
                 <li>优化显示顺序orderNum类型为整型</li>
@@ -528,8 +539,12 @@
                 <li>BLOB下载时清除URL对象引用</li>
                 <li>代码生成导入表按创建时间排序</li>
                 <li>修复代码生成页面数据编辑保存之后总是跳转第一页的问题</li>
-                <li>修复带safari浏览器无法格式化utc日期格式yyyy-MM-dd'T'HH:mm:ss.SSS问题</li>
-                <li>多图上传组件移除多余的api地址&验证失败导致图片删除问题&无法删除相应图片修复</li>
+                <li>
+                  修复带safari浏览器无法格式化utc日期格式yyyy-MM-dd'T'HH:mm:ss.SSS问题
+                </li>
+                <li>
+                  多图上传组件移除多余的api地址&验证失败导致图片删除问题&无法删除相应图片修复
+                </li>
                 <li>其他细节优化</li>
               </ol>
             </el-collapse-item>
@@ -574,7 +589,9 @@
             </el-collapse-item>
             <el-collapse-item title="v3.5.0 - 2021-05-25">
               <ol>
-                <li>新增菜单导航显示风格TopNav（false为左侧导航菜单，true为顶部导航菜单）</li>
+                <li>
+                  新增菜单导航显示风格TopNav（false为左侧导航菜单，true为顶部导航菜单）
+                </li>
                 <li>布局设置支持保存&重置配置</li>
                 <li>修复树表数据显示不全&加载慢问题</li>
                 <li>新增IE浏览器版本过低提示页面</li>
@@ -593,7 +610,9 @@
                 <li>升级druid到最新版本v1.2.6</li>
                 <li>升级mybatis到最新版3.5.6 阻止远程代码执行漏洞</li>
                 <li>升级oshi到最新版本v5.6.0</li>
-                <li>velocity剔除commons-collections版本，防止3.2.1版本的反序列化漏洞</li>
+                <li>
+                  velocity剔除commons-collections版本，防止3.2.1版本的反序列化漏洞
+                </li>
                 <li>数据监控页默认账户密码防止越权访问</li>
                 <li>修复firefox下表单构建拖拽会新打卡一个选项卡</li>
                 <li>修正后端导入表权限标识</li>
@@ -975,7 +994,7 @@
             <img
               src="@/assets/images/pay.png"
               alt="donate"
-              style="width:100%"
+              style="width: 100%"
             />
             <span style="display: inline-block; height: 30px; line-height: 30px"
               >你可以请作者喝杯咖啡表示鼓励</span
@@ -988,10 +1007,10 @@
 </template>
 
 <script setup name="Index">
-const version = ref('3.8.7')
+const version = ref("3.8.7");
 
 function goTarget(url) {
-  window.open(url, '__blank')
+  window.open(url, "__blank");
 }
 </script>
 
@@ -1058,4 +1077,3 @@ function goTarget(url) {
   }
 }
 </style>
-
