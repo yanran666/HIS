4c837dd208168b9172203c1ece418831d059d7df
sonicyouth7 <1625159433@qq.com>
2024-07-08 17:02:59 +0800

    挂号和退号前后端

--- YGT/web/RuoYi-Vue3-master/src/views/appointment/appointment/add.vue
+++ YGT/web/RuoYi-Vue3-master/src/views/appointment/appointment/add.vue
@@ -12,9 +12,9 @@
         <el-col :span="24">
           <el-row gutter="15">
             <el-col :span="8">
-              <el-form-item label="病历号" prop="field104">
+              <el-form-item label="病历号" prop="medicalRecordNumber">
                 <el-input
-                  v-model="formData.field104"
+                  v-model="formData.medicalRecordNumber"
                   type="text"
                   placeholder="请输入病历号"
                   clearable
@@ -23,9 +23,9 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="姓名" prop="field103">
+              <el-form-item label="姓名" prop="name">
                 <el-input
-                  v-model="formData.field103"
+                  v-model="formData.name"
                   type="text"
                   placeholder="请输入姓名"
                   clearable
@@ -34,15 +34,15 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="性别" prop="field102">
+              <el-form-item label="性别" prop="gender">
                 <el-select
-                  v-model="formData.field102"
+                  v-model="formData.gender"
                   placeholder="请输入性别"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field102Options"
+                    v-for="(item, index) in genderOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -56,9 +56,9 @@
         <el-col :span="24">
           <el-row gutter="15">
             <el-col :span="8">
-              <el-form-item label="年龄" prop="field106">
+              <el-form-item label="年龄" prop="age">
                 <el-input
-                  v-model="formData.field106"
+                  v-model="formData.age"
                   type="text"
                   placeholder="请输入年龄"
                   clearable
@@ -67,10 +67,10 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="出生日期" prop="field107">
+              <el-form-item label="出生日期" prop="dob">
                 <el-date-picker
                   type="text"
-                  v-model="formData.field107"
+                  v-model="formData.dob"
                   format="YYYY-MM-DD"
                   value-format="YYYY-MM-DD"
                   :style="{ width: '100%' }"
@@ -80,9 +80,9 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="身份证号" prop="field108">
+              <el-form-item label="身份证号" prop="idNumber">
                 <el-input
-                  v-model="formData.field108"
+                  v-model="formData.idNumber"
                   type="text"
                   placeholder="请输入身份证号"
                   clearable
@@ -95,9 +95,9 @@
         <el-col :span="23">
           <el-row gutter="15">
             <el-col :span="24">
-              <el-form-item label="家庭住址" prop="field110">
+              <el-form-item label="家庭住址" prop="address">
                 <el-input
-                  v-model="formData.field110"
+                  v-model="formData.address"
                   type="text"
                   placeholder="请输入家庭住址"
                   clearable
@@ -110,15 +110,15 @@
         <el-col :span="23">
           <el-row gutter="15">
             <el-col :span="8">
-              <el-form-item label="结算类别" prop="field113">
+              <el-form-item label="结算类别" prop="settlementType">
                 <el-select
-                  v-model="formData.field113"
+                  v-model="formData.settlementType"
                   placeholder="请选择结算类别"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field113Options"
+                    v-for="(item, index) in settlementTypeOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -128,9 +128,9 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="看诊日期" prop="field115">
+              <el-form-item label="看诊日期" prop="clinicDate">
                 <el-date-picker
-                  v-model="formData.field115"
+                  v-model="formData.clinicDate"
                   format="YYYY-MM-DD"
                   value-format="YYYY-MM-DD"
                   :style="{ width: '100%' }"
@@ -141,16 +141,16 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="午别" prop="field114">
+              <el-form-item label="午别" prop="session">
                 <el-select
-                  v-model="formData.field114"
+                  v-model="formData.session"
                   placeholder="请选择午别"
                   :disabled="true"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field114Options"
+                    v-for="(item, index) in sessionOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -164,15 +164,15 @@
         <el-col :span="23">
           <el-row gutter="15">
             <el-col :span="8">
-              <el-form-item label="挂号科室" prop="field120">
+              <el-form-item label="挂号科室" prop="department">
                 <el-select
-                  v-model="formData.field120"
+                  v-model="formData.department"
                   placeholder="请选择挂号科室"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field120Options"
+                    v-for="(item, index) in departmentOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -182,15 +182,15 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="号别" prop="field136">
+              <el-form-item label="号别" prop="registrationType">
                 <el-select
-                  v-model="formData.field136"
+                  v-model="formData.registrationType"
                   placeholder="请输入号别"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field136Options"
+                    v-for="(item, index) in registrationTypeOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -200,15 +200,15 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="看诊医生" prop="field122">
+              <el-form-item label="看诊医生" prop="attendingDoctor">
                 <el-select
-                  v-model="formData.field122"
+                  v-model="formData.attendingDoctor"
                   placeholder="请选择看诊医生"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field122Options"
+                    v-for="(item, index) in attendingDoctorOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -222,9 +222,9 @@
         <el-col :span="23">
           <el-row gutter="15">
             <el-col :span="8">
-              <el-form-item label="初始号额" prop="field124">
+              <el-form-item label="初始号额" prop="initialQuota">
                 <el-input
-                  v-model="formData.field124"
+                  v-model="formData.initialQuota"
                   type="text"
                   placeholder="请选择初始号额"
                   clearable
@@ -233,9 +233,9 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="已用号额" prop="field138">
+              <el-form-item label="已用号额" prop="usedQuota">
                 <el-input
-                  v-model="formData.field138"
+                  v-model="formData.usedQuota"
                   type="text"
                   placeholder="请输入已用号额"
                   clearable
@@ -244,8 +244,8 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="病历本" prop="field126" required>
-                <el-switch v-model="formData.field126"></el-switch>
+              <el-form-item label="病历本" prop="medicalRecordStatus" required>
+                <el-switch v-model="formData.medicalRecordStatus"></el-switch>
               </el-form-item>
             </el-col>
           </el-row>
@@ -253,9 +253,9 @@
         <el-col :span="23">
           <el-row gutter="15">
             <el-col :span="8">
-              <el-form-item label="应收金额" prop="field128">
+              <el-form-item label="应收金额" prop="receivableAmount">
                 <el-input
-                  v-model="formData.field128"
+                  v-model="formData.receivableAmount"
                   type="text"
                   placeholder="请选择应收金额"
                   clearable
@@ -264,15 +264,15 @@
               </el-form-item>
             </el-col>
             <el-col :span="8">
-              <el-form-item label="收费方式" prop="field130">
+              <el-form-item label="收费方式" prop="paymentMethod">
                 <el-select
-                  v-model="formData.field130"
+                  v-model="formData.paymentMethod"
                   placeholder="请选择收费方式"
                   clearable
                   :style="{ width: '100%' }"
                 >
                   <el-option
-                    v-for="(item, index) in field130Options"
+                    v-for="(item, index) in paymentMethodOptions"
                     :key="index"
                     :label="item.label"
                     :value="item.value"
@@ -296,100 +296,100 @@
 <script setup>
 import { ElMessage } from "element-plus";
 const formRef = ref();
-import { addAppointment } from '@/api/appointment';
+import { addAppointment } from "@/api/appointment/appointment";
 const data = reactive({
   formData: {
-    field104: undefined,
-    field103: undefined,
-    field102: 2,
-    field106: undefined,
-    field107: null,
-    field108: undefined,
-    field110: undefined,
-    field113: undefined,
-    field115: "2024-07-20",
-    field114: 1,
-    field120: undefined,
-    field136: 1,
-    field122: undefined,
-    field124: 1,
-    field138: 0,
-    field126: true,
-    field128: 50,
-    field130: 1,
+    medicalRecordNumber: undefined,
+    name: undefined,
+    gender: 2,
+    age: undefined,
+    dob: null,
+    idNumber: undefined,
+    address: undefined,
+    settlementType: undefined,
+    clinicDate: "2024-07-20",
+    session: 1,
+    department: undefined,
+    registrationType: 1,
+    attendingDoctor: undefined,
+    initialQuota: 1,
+    usedQuota: 0,
+    medicalRecordStatus: true,
+    receivableAmount: 50,
+    paymentMethod: 1,
   },
   rules: {
-    field104: [
+    medicalRecordNumber: [
       {
         required: true,
         message: "请输入病历号",
         trigger: "blur",
       },
     ],
-    field103: [
+    name: [
       {
         required: true,
         message: "请输入姓名",
         trigger: "blur",
       },
     ],
-    field102: [
+    gender: [
       {
         required: true,
         message: "请输入性别",
         trigger: "change",
       },
     ],
-    field106: [],
-    field107: [],
-    field108: [
+    age: [],
+    dob: [],
+    idNumber: [
       {
         required: true,
         message: "请输入身份证号",
         trigger: "blur",
       },
     ],
-    field110: [],
-    field113: [],
-    field115: [
+    address: [],
+    settlementType: [],
+    clinicDate: [
       {
         required: true,
         message: "请选择看诊日期",
         trigger: "change",
       },
     ],
-    field114: [],
-    field120: [
+    session: [],
+    department: [
       {
         required: true,
         message: "请选择挂号科室",
         trigger: "change",
       },
     ],
-    field136: [],
-    field122: [],
-    field124: [
+    registrationType: [],
+    attendingDoctor: [],
+    initialQuota: [
       {
         required: true,
         message: "请选择初始号额",
         trigger: "blur",
       },
     ],
-    field138: [
+    usedQuota: [
       {
         required: true,
         message: "请输入已用号额",
         trigger: "blur",
       },
     ],
-    field128: [
+    receivableAmount: [
       {
         required: true,
         message: "请选择应收金额",
         trigger: "blur",
       },
     ],
-    field130: [
+    paymentMethod: [
       {
         required: true,
         message: "请选择收费方式",
@@ -399,7 +399,7 @@ const data = reactive({
   },
 });
 const { formData, rules } = toRefs(data);
-const field102Options = ref([
+const genderOptions = ref([
   {
     label: "男",
     value: 1,
@@ -409,7 +409,7 @@ const field102Options = ref([
     value: 2,
   },
 ]);
-const field113Options = ref([
+const settlementTypeOptions = ref([
   {
     label: "自费",
     value: 1,
@@ -419,7 +419,7 @@ const field113Options = ref([
     value: 2,
   },
 ]);
-const field114Options = ref([
+const sessionOptions = ref([
   {
     label: "上午",
     value: 1,
@@ -429,7 +429,7 @@ const field114Options = ref([
     value: 2,
   },
 ]);
-const field120Options = ref([
+const departmentOptions = ref([
   {
     label: "选项一",
     value: 1,
@@ -439,7 +439,7 @@ const field120Options = ref([
     value: 2,
   },
 ]);
-const field136Options = ref([
+const registrationTypeOptions = ref([
   {
     label: "普通号",
     value: 1,
@@ -449,17 +449,17 @@ const field136Options = ref([
     value: 2,
   },
 ]);
-const field122Options = ref([
+const attendingDoctorOptions = ref([
   {
-    label: "上午",
+    label: "101",
     value: 1,
   },
   {
-    label: "下午",
+    label: "102",
     value: 2,
   },
 ]);
-const field130Options = ref([
+const paymentMethodOptions = ref([
   {
     label: "医保卡",
     value: 1,
@@ -480,11 +480,11 @@ function submitForm() {
     // 提交表单
     addAppointment(formData.value)
       .then((response) => {
-        ElMessage.success('挂号成功');
+        ElMessage.success("挂号成功");
         resetForm();
       })
       .catch((error) => {
-        ElMessage.error('挂号失败: ' + error.message);
+        ElMessage.error("挂号失败: " + error.message);
       });
   });
 }
