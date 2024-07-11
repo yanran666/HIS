c2dc07c7d66704cf50887414df51114f15e61350
sonicyouth7 <1625159433@qq.com>
2024-07-11 14:26:09 +0800

    主页

--- YGT/web/RuoYi-Vue3-master/src/views/index.vue
+++ YGT/web/RuoYi-Vue3-master/src/views/index.vue
@@ -1,32 +1,51 @@
 <template>
-  <div class="block text-center" m="t-4">
-    <span class="demonstration">Switch when indicator is clicked</span>
-    <el-carousel trigger="click" height="650px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+  <div class="block text-center" style="text-align: center">
+    <el-carousel height="385px">
+      <el-carousel-item v-for="item in imageUrl" :key="item">
+        <img :src="item.url" alt="" class="image" />
       </el-carousel-item>
     </el-carousel>
+    <div>
+      <el-card class="index">
+        <index></index>
+      </el-card>
+    </div>
   </div>
+  <el-card style="max-width: 520px" class="calendar">
+    <el-calendar v-model="value" />
+  </el-card>
 </template>
 
-<style scoped>
-.demonstration {
-  color: var(--el-text-color-secondary);
+<script setup lang="ts">
+import { ref } from "vue";
+import index from "E:/GitHub/HIS/YGT/web/RuoYi-Vue3-master/src/views/monitor/operlog/index.vue";
+const value = ref(new Date());
+const imageUrl = [
+  {
+    url: "https://imgs.699pic.com/images/402/164/589.jpg!seo.v1",
+  },
+  {
+    url: "https://imgs.699pic.com/images/401/954/086.jpg!seo.v1",
+  },
+  {
+    url: "https://imgs.699pic.com/images/402/164/597.jpg!seo.v1",
+  },
+  {
+    url: "https://imgs.699pic.com/images/402/144/418.jpg!seo.v1",
+  },
+];
+</script>
+<style>
+.image {
+  width: 1100px;
 }
-
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  line-height: 150px;
-  margin: 0;
-  text-align: center;
+.calendar {
+  margin: auto;
+  float: right;
+  height: 530px;
 }
-
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+.index {
+  width: 1000px;
+  float: left;
 }
 </style>
